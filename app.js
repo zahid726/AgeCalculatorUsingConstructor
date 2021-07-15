e6cd5fc220524c8b65a100cde47831f2c10d159b
zahid726 <zahid.nagri.edu.pk@gmail.com>
2021-07-15 18:14:36 +0500

    updated prototype

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-let Pname= document.getElementById('name');
+let fname= document.getElementById('fname');
+let lname= document.getElementById('lname');
 let dob= document.getElementById('dob');
 let button= document.getElementById('button')
-let outputName= document.getElementById('outputName');
+let outputfName= document.getElementById('outputfName');
+let outputlName= document.getElementById('outputlName');
 let outAge= document.getElementById('outputAge')
 const display3 =document.getElementById('display');
 display3.style.display='none';
 
 button.addEventListener('click',function(){
-   let Name= Pname.value;
+   let firstName= fname.value;
+   let lastName= lname.value;
    let Dob= dob.value
-   function Person(name, dob) {
-      this.Name = name;
+   function Person(firstName, lastName, dob) {
+      this.fname = firstName;
+      this.lname= lastName;
       this.dateOfBirth= new Date(dob);
-      this.calculateAge= function(){
-         const difference= Date.now() - this.dateOfBirth.getTime();
-         const ageDate= new Date(difference);
-         return ageDate.getFullYear() - 1970;
-   
-      }
+     
 
    }
-   const hussain = new Person("hussain",'5 june 2010')
-   const newperosn= new Person(Name, Dob)
+   Person.prototype.calculateAge= function(){
+      const difference= Date.now() - this.dateOfBirth.getTime();
+      const ageDate= new Date(difference);
+      return ageDate.getFullYear() - 1970;
+   }
+   Person.prototype.fullName= function(){
+      return `${this.fname} ${this.lname}`
+   }
+   // const hussain = new Person("hussain",'5 june 2010')
+   const newperosn= new Person(firstName,lastName, Dob)
  display3.style.display="block";
    
-   outputName.textContent= Name;
-   outAge.textContent=newperosn.calculateAge()
+   outputfName.textContent= newperosn.fullName();
+   outAge.textContent=newperosn.calculateAge();
 })
 
 
